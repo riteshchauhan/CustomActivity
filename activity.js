783c032eaa1419cab77e8d79dc16a048a4e4388d
Ritesh Chauhan <ritesh.chauhan@riteshc-ltm.internal.salesforce.com>
2020-08-15 11:47:05 +0530

    additional changes

---
+++ activity.js
@@ -0,0 +1,15 @@
+
+var Postmonger = require('postmonger');
+
+var connection = new Postmonger.session();
+
+connection.trigger('ready');
+
+connection.on('initActivity', function( data ){
+document.getElementById('Configuration').value = JSON.stringify(data,null,2);
+});
+
+connection.on('clickedNext', function(){
+var Configuration= JSON.parse(document.getElementById('Configuration').value);
+connection.trigger('updateActivity', Configuration);
+});
